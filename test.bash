c19891ebb7f6b896a8feeb37a5d028bd5a03350c
suzuki takuma <s23c1076vc@s.chibakoudai.jp>
2024-12-03 14:05:01 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -10,13 +10,13 @@ ng () {
 res=0
 
 #正常なとき
-out=$(echo "10000 10" | python3 ./tiac.py)
+out=$(echo "10000 10" | python3 ./tiac)
 if [[ "$out" != *"税込み: 11000.00円"* ]]; then
     ng "$LINENO"
 fi
 
 #不正な入力なとき
-out=$(echo "not_a_number 10" | python3 ./tiac.py 2>&1)
+out=$(echo "not_a_number 10" | python3 ./tiac. 2>&1)
 if [[ "$out" != *"エラー: 金額と消費税率は数値で指定してください"* ]]; then
     ng "$LINENO"
 fi
